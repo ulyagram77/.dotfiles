a23a3ab4ab493bb51960bc4d8097159145af5895
Kyrylo Ulianov <74236970+ulyagram77@users.noreply.github.com>
2023-12-29 20:23:23 +0200

    Delete .config directory

--- .config/qtile/scripts/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# glava &
-dunst &
-# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/polkit-kde-authentication-agent-1 &
-flatpak run com.google.Chrome &
-code &
-discord &
-telegram-desktop &
