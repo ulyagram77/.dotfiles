c8ae7363ef6d703bfa04d0b1c85d0ac59c8af7a5
whitegolyb <kirillulianow@gmail.com>
2023-12-29 20:17:49 +0200

    initial commit

---
+++ .config/qtile/scripts/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# glava &
+dunst &
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/polkit-kde-authentication-agent-1 &
+flatpak run com.google.Chrome &
+code &
+discord &
+telegram-desktop &
