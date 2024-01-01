f37b0944ea0c5d301472c1356ae7cc67a90e8ec9
whitegolyb <kirillulianow@gmail.com>
2024-01-01 23:50:11 +0200

    added windows configs

---
+++ Public/WindowsOS/windows-terminal/oh-my-posh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/kirill.omp.json" | Invoke-Expression
+Import-Module -Name Terminal-Icons
+# C:\Users\kiril\AppData\Local\Programs\oh-my-posh\themes
